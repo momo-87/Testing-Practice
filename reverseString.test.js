6232fb5f5711da1b6b5eecccac1125aeb626e586
Momo-87 <momochristian87@gmail.com>
2023-05-10 14:47:58 +0200

    reverseString function written with one testing fucntion

---
+++ reverseString.test.js
@@ -0,0 +1,7 @@
+const reverseString = require('./reverseString');
+
+test("reverseString('Christian) is eaqual to 'naitsirhC'", () => {
+    expect(reverseString('Christian')).toBe('naitsirhC');
+})
+
+
